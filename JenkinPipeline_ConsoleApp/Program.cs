f5bb86dcfea630dac8e410b6897dc4f26b2902b9
Shivam Kushwaha <shivam.kushwaha@nagarro.com>
2023-01-11 06:34:35 +0530

    first commit

---
+++ JenkinPipeline_ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JenkinPipeline_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
